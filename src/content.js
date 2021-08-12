5940aa7b20fa675aac7f735fd877ea4d30e260be
Alphka <kayo.felipe.souza2014@gmail.com>
2021-08-12 17:11:21 -0300

    Remove popups

--- src/content.js
+++ src/content.js
@@ -27,15 +27,28 @@ if(location.hostname === "brainly.com.br"){
 		"div[data-testid=\"brainly_plus_toaster_wrapper\"]",
 		"button[data-test=\"navigation_payments\"]",
 		"div.brn-brainly-plus-box",
-		"div#new-ad-placeholder-question-desktop"
+		"div#new-ad-placeholder-question-desktop",
+		"div.brn-ads-box",
+		"div.js-react-registration-toplayer",
+		"div.sg-overlay.sg-overlay--dark"
 	]
 
-	selectors.forEach(selector => waitElement(selector, 1e4).then(element => element.remove()).catch(() => {}))
+	selectors.forEach(selector => waitElement(selector).then(element => element.remove()).catch(() => {}))
 	Object.getOwnPropertyNames(localStorage).forEach(name => name.includes("funnel") && localStorage.removeItem(name))
 
 	if(!document.cookie.includes("cookieconsent_dismissed")) document.cookie = "cookieconsent_dismissed=yes; max-age=31536000; path=/; samesite=Lax"
 	if(!localStorage.getItem("registration-toplayer/expires")){
-		localStorage.setItem("registration-toplayer/expires", Date.now())
+		localStorage.setItem("registration-toplayer/expires", Date.now() + 3.6e6)
 		localStorage.setItem("registration-toplayer/cursor", 0)
 	}
 }
+
+const bodyMutation = new MutationObserver((mutations, observer) => {
+	mutations.forEach(mutation => {
+		if(mutation.type === "attributes" && mutation.attributeName === "style" && document.body.style.overflow) document.body.style.overflow = "auto"
+	})
+})
+
+bodyMutation.observe(document.body, {
+	attributes: true
+})
