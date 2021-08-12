76c483ee85193f6996d04d73b7877b424d360f13
Alphka <kayo.felipe.souza2014@gmail.com>
2021-08-12 18:48:29 -0300

    Add mutation observer

--- src/content.js
+++ src/content.js
@@ -1,3 +1,17 @@
+const brainlyHostnames = [
+	"brainly.com.br",
+	"brainly.com",
+	"brainly.ph",
+	"brainly.co.id",
+	"brainly.ro",
+	"eodev.com",
+	"brainly.lat",
+	"znanija.com",
+	"brainly.pl",
+	"brainly.in",
+	"nosdevoirs.fr"
+]
+
 function waitElement(selector, max){
 	const begin = Date.now()
 	return new Promise((resolve, reject) => {
@@ -7,11 +21,33 @@ function waitElement(selector, max){
 				resolve(element)
 				clearInterval(interval)
 			}else if(max && Date.now() - begin > max) reject()
-		})
+		}, 10)
 	})
 }
 
-if(location.hostname === "brainly.com.br"){
+if(brainlyHostnames.includes(location.hostname)){
+	Object.getOwnPropertyNames(localStorage).forEach(name => name.includes("funnel") && localStorage.removeItem(name))
+
+	if(!document.cookie.includes("cookieconsent_dismissed")) document.cookie = "cookieconsent_dismissed=yes; max-age=31536000; path=/; samesite=Lax"
+	
+	if(!localStorage.getItem("registration-toplayer/expires")){
+		localStorage.setItem("registration-toplayer/expires", Date.now() + 3.6e6)
+		localStorage.setItem("registration-toplayer/cursor", 0)
+	}
+
+	if(location.hostname === "nosdevoirs.fr" && !localStorage.getItem("truste.eu.cookie.notice_preferences")){
+		let date = new Date()
+		date.setFullYear(date.getFullYear() + 1)
+		date = date.getTime()
+
+		localStorage.setItem("notice_preferences", "2:")
+		localStorage.setItem("notice_gdpr_prefs", "0,1,2:")
+		localStorage.setItem("truste.eu.cookie.notice_preferences", `{"name":"truste.eu.cookie.notice_preferences","value":"2:","path":"/","expires":${date}}`)
+		localStorage.setItem("truste.eu.cookie.notice_gdpr_prefs", `{"name":"truste.eu.cookie.notice_gdpr_prefs","value":"0,1,2:","path":"/","expires":${date}}`)
+		localStorage.setItem("truste.eu.cookie.cmapi_cookie_privacy", `{"name":"truste.eu.cookie.cmapi_cookie_privacy","value":"permit 1,2,3","path":"/","expires":${date}}`)
+		localStorage.setItem("truste.eu.cookie.cmapi_gtm_bl", `{"name":"truste.eu.cookie.cmapi_gtm_bl","value":"","path":"/","expires":${date}}`)
+	}
+
 	const selectors = [
 		"div.section--lnnYy.section--3Yobl",
 		"div[class*=payments-section]",
@@ -30,25 +66,27 @@ if(location.hostname === "brainly.com.br"){
 		"div#new-ad-placeholder-question-desktop",
 		"div.brn-ads-box",
 		"div.js-react-registration-toplayer",
-		"div.sg-overlay.sg-overlay--dark"
-	]
-
-	selectors.forEach(selector => waitElement(selector).then(element => element.remove()).catch(() => {}))
-	Object.getOwnPropertyNames(localStorage).forEach(name => name.includes("funnel") && localStorage.removeItem(name))
-
-	if(!document.cookie.includes("cookieconsent_dismissed")) document.cookie = "cookieconsent_dismissed=yes; max-age=31536000; path=/; samesite=Lax"
-	if(!localStorage.getItem("registration-toplayer/expires")){
-		localStorage.setItem("registration-toplayer/expires", Date.now() + 3.6e6)
-		localStorage.setItem("registration-toplayer/cursor", 0)
-	}
-}
-
-const bodyMutation = new MutationObserver((mutations, observer) => {
-	mutations.forEach(mutation => {
-		if(mutation.type === "attributes" && mutation.attributeName === "style" && document.body.style.overflow) document.body.style.overflow = "auto"
+		"div[class*=SubscriptionInfo__container]",
+		"div.truste_overlay",
+		"div.truste_box_overlay_inner",
+		"div.truste_box_overlay"
+	],
+	bodyMutation = new MutationObserver((mutations, observer) => {
+		mutations.forEach(mutation => {
+			if(mutation.type === "attributes"){
+				if(mutation.attributeName === "style" && document.body.style.overflow) document.body.style.overflow = "auto"
+			}else selectors.forEach(selector => {
+				const elements = document.querySelectorAll(selector)
+				if(elements.length) Array.from(elements).forEach(element => element.remove())
+			})
+		})
 	})
-})
+	
+	bodyMutation.observe(document.body, {
+		attributes: true,
+		childList: true,
+		subtree: true
+	})
 
-bodyMutation.observe(document.body, {
-	attributes: true
-})
+	waitElement("div.sg-overlay.sg-overlay--dark").then(element => element.remove())
+}
